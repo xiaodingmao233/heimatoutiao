f54d2c2434da6a37432766f348aab08c7aed720d
lizheng <2112276441@qq.com>
2021-02-20 15:02:23 +0800

    展示文章评论管理列表

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -9,23 +9,42 @@
       </div>
       <el-table
         class="table-list"
-        :data="tableData"
+        :data="articles"
         style="width: 100%"
         stripe
       >
         <el-table-column
-          prop="date"
-          label="日期"
-          width="180">
+          prop="title"
+          label="标题"
+        >
         </el-table-column>
         <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
+          prop="total_comment_count"
+          label="总评论数"
+        >
         </el-table-column>
         <el-table-column
-          prop="address"
-          label="地址">
+          prop="fans_comment_count"
+          label="粉丝评论数"
+        >
+        </el-table-column>
+        <el-table-column
+          label="状态"
+        >
+          <template slot-scope="scope">
+            {{ scope.row.comment_status ? '正常' : '关闭' }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作"
+        >
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.comment_status"
+              active-color="#13ce66"
+              inactive-color="#ff4949">
+            </el-switch>
+          </template>
         </el-table-column>
       </el-table>
       <el-pagination
