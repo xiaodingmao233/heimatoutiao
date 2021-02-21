21368d6f326b865aba33e2e2fcba961f2ab128e1
lizheng <2112276441@qq.com>
2021-02-21 19:35:27 +0800

    初始化裁切器

--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -35,15 +35,36 @@
             <el-avatar shape="square" :size="150" fit="cover" :src="user.photo"></el-avatar>
           </label>
           <p style="cursor: pointer;" @click="$refs.file.click()">点击修改头像</p>
-          <input id="file" type="file" hidden ref="file">
+          <input id="file" type="file" hidden ref="file" @change="onFileChange">
         </el-col>
       </el-row>
     </el-card>
+    <el-dialog
+      title="修改头像"
+      :visible.sync="dialogVisible"
+      append-to-body
+      @opened="onDialogOpened"
+    >
+      <div class="preview-image-wrap">
+        <img
+          class="preview-image"
+          :src="perviewImage"
+          ref="preview-image"
+        >
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { getUserProfile } from '@/api/user'
+import 'cropperjs/dist/cropper.css'
+import Cropper from 'cropperjs'
+
 export default {
   name: 'SettingsIndex',
   components: {},
@@ -67,7 +88,9 @@ export default {
         mobile: '',
         name: '',
         photo: ''
-      }
+      },
+      dialogVisible: false,
+      perviewImage: ''
     }
   },
   computed: {},
@@ -85,11 +108,45 @@ export default {
         // console.log(res)
         this.user = res.data.data
       })
+    },
+    onFileChange () {
+      const file = this.$refs.file
+
+      const blobData = window.URL.createObjectURL(file.files[0])
+
+      this.perviewImage = blobData
+
+      this.dialogVisible = true
+      // 解决选择相同文件不触发 change 事件的问题
+      this.$refs.file.value = ''
+    },
+    onDialogOpened () {
+      const image = this.$refs['preview-image']
+
+      const cropper = new Cropper(image, {
+        aspectRatio: 16 / 9,
+        crop (event) {
+          console.log(event.detail.x)
+          console.log(event.detail.y)
+          console.log(event.detail.width)
+          console.log(event.detail.height)
+          console.log(event.detail.rotate)
+          console.log(event.detail.scaleX)
+          console.log(event.detail.scaleY)
+        }
+      })
+      console.log(cropper)
     }
   }
 }
 </script>
 
 <style scoped lang="less">
-
+.preview-image-wrap {
+  .preview-image {
+    display: block;
+    max-width: 100%;
+    height: 200px;
+  }
+}
 </style>
