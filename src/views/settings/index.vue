f208ae7d0a692de4ef6d58bc0fc82242238d17c3
lizheng <2112276441@qq.com>
2021-02-21 13:26:01 +0800

    个人设置页面布局

--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="settings-container">
-    个人设置
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>个人设置</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-row>
+        <el-col span="12">
+          <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="编号">
+              123
+            </el-form-item>
+            <el-form-item label="手机">
+              456
+            </el-form-item>
+            <el-form-item label="媒体名称">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="媒体介绍">
+              <el-input type="textarea" v-model="form.desc"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+        <el-col span="4" offset="4">
+          <el-avatar shape="square" :size="150" fit="cover" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"></el-avatar>
+          <p>点击修改头像</p>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
@@ -11,14 +46,27 @@ export default {
   props: {},
   data () {
     return {
-
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
     }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
