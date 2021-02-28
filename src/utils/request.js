31e967eda2ebf2af7af6743701b8211c3419cd17
lizheng <2112276441@qq.com>
2021-02-28 16:45:33 +0800

    功能优化--处理公共状态码

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import JSONbig from 'json-bigint'
 import router from '@/router'
+import { Message } from 'element-ui'
 
 const request = axios.create({
   // baseURL: 'http://ttapi.research.itcast.cn/',
@@ -38,14 +39,27 @@ request.interceptors.response.use(function (response) {
   // 注意 一定要把响应结果 return 否则真正发送请求的位置拿不到数据
   return response
 }, function (error) {
+  const { status } = error.response
   // 任何超出 2xx 的响应码都会进入这里
-  console.log('状态码异常')
-  console.dir(error)
-  // 跳转到登录页面
-  // 清楚本地存储中的用户登录状态
-  window.localStorage.removeItem('user')
-  router.push('/login')
+  // console.log('状态码异常')
+  // console.dir(error)
+  if (status === 401) {
+    // 跳转到登录页面
+    // 清楚本地存储中的用户登录状态
+    window.localStorage.removeItem('user')
+    router.push('/login')
+    Message.error('登录状态无效，请重新登录')
+  } else if (status === 403) {
+    // token 未携带或已过期
+    Message.error('没有操作权限')
+  } else if (status === 400) {
+    // 客户端参数错误
+    Message.error('参数错误，请检查请求参数')
+  } else if (status >= 500) {
+    Message.error('服务端内部异常，请稍后重试')
+  }
   return Promise.reject(error)
 })
 
+// 导出请求方法
 export default request
